999d6fbb7bbc7b04f58cf6630d4f04e7aeae8594
augusto <augustosaaude@gmail.com>
2023-01-12 15:22:21 -0300

    refactor: refatorei o código para deixa-lo mais legivel

--- app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
@@ -26,29 +26,31 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun numberGenerator(text: String, txtResult: TextView) {
-        if(text.isNotEmpty()){
-            val qtd = text.toInt()
 
-            if(qtd >= 6 && qtd <= 15) {
-                val numbers = mutableSetOf<Int>()
-                val random = Random()
-
-                while(true) {
-                    val number = random.nextInt(60)
-                    numbers.add(number + 1)
-
-                    if(numbers.size == qtd) {
-                        break
-                    }
-                }
-
-                txtResult.text = numbers.joinToString(" - ")
-
-            } else {
-                Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show()
-            }
-        } else {
+        if (text.isEmpty()) {
             Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show()
+            return
         }
+
+        val qtd = text.toInt()
+
+        if (qtd < 6 || qtd > 15) {
+            Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val numbers = mutableSetOf<Int>()
+        val random = Random()
+
+        while (true) {
+            val number = random.nextInt(60)
+            numbers.add(number + 1)
+
+            if (numbers.size == qtd) {
+                break
+            }
+        }
+
+        txtResult.text = numbers.joinToString(" - ")
     }
 }
